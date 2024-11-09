bfc389f1c648cbb12571ce1abbc73583dc817222
Anmol <102401768+avdvh@users.noreply.github.com>
2024-11-09 10:36:58 +0530

    Update solver.js

--- solver.js
+++ solver.js
@@ -1,27 +1,26 @@
 function isSafe(board, row, col, num) {
-    for (let x = 0; x < 9; x++) {
-      if (board[row][x] === num || board[x][col] === num || board[3 * Math.floor(row / 3) + Math.floor(x / 3)][3 * Math.floor(col / 3) + x % 3] === num) {
+  for (let x = 0; x < 9; x++) {
+    if (board[row][x] === num || board[x][col] === num || board[3 * Math.floor(row / 3) + Math.floor(x / 3)][3 * Math.floor(col / 3) + x % 3] === num) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function solveSudoku(board) {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (board[row][col] === 0) {
+        for (let num = 1; num <= 9; num++) {
+          if (isSafe(board, row, col, num)) {
+            board[row][col] = num;
+            if (solveSudoku(board)) return true;
+            board[row][col] = 0;
+          }
+        }
         return false;
       }
     }
-    return true;
   }
-  
-  function solveSudoku(board) {
-    for (let row = 0; row < 9; row++) {
-      for (let col = 0; col < 9; col++) {
-        if (board[row][col] === 0) {
-          for (let num = 1; num <= 9; num++) {
-            if (isSafe(board, row, col, num)) {
-              board[row][col] = num;
-              if (solveSudoku(board)) return true;
-              board[row][col] = 0;
-            }
-          }
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-  
+  return true;
+}
